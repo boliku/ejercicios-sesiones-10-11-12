53ecc9fdd6950cd09345b589651074a2ee20f2e1
boliku <boli@live.com.ar>
2022-08-28 21:30:35 -0300

    Primer Subida

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Square from './components/pure/Square';
+
+function App() {
+  return (
+    <div className="App">
+      <Square />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pure/Square.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../../styles/Square.css';
+
+const Square = () => {
+
+  const changeColor = () => {
+    
+  };
+
+  return (
+    <div>
+      <div className="square">
+      </div>
+    </div>
+  );
+};
+
+export default Square;
